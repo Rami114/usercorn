6c4c3e457e028d7dd27b5bb3492b57d93db74863
Grazfather <grazfather@gmail.com>
2017-05-15 18:54:03 -0700

    debug: Add write string and write value commands (es and e)

--- go/debug/cmd/mem.go
+++ go/debug/cmd/mem.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"errors"
+	"strings"
+
 	"github.com/lunixbochs/usercorn/go/models"
 )
 
@@ -18,7 +21,7 @@ var MapsCmd = registerCommand(&Command{
 
 var MemCmd = registerCommand(&Command{
 	Name: "mem",
-	Desc: "Read/write memory.",
+	Desc: "Dump memory.",
 	// TODO: need overloading so we can keep arg safety
 	// at that point optional args might as well be an overloaded form
 	Run: func(c *Context, addr, size uint64) error {
@@ -28,6 +31,45 @@ var MemCmd = registerCommand(&Command{
 		}
 		for _, line := range models.HexDump(addr, mem, int(c.U.Bits())) {
 			c.Printf("  %s\n", line)
+		}
+		return nil
+	},
+})
+
+var WriteCmd = registerCommand(&Command{
+	Name: "e",
+	Desc: "Write value to memory.",
+	// TODO: Need the right codec to allow optional args
+	Run: func(c *Context, addr, value, optSize uint64) error {
+		s := c.U.StrucAt(addr)
+		var err error
+		switch optSize {
+		case 8:
+			err = s.Pack(uint64(value))
+		case 4:
+			err = s.Pack(uint32(value))
+		case 2:
+			err = s.Pack(uint16(value))
+		case 1:
+			err = s.Pack(uint8(value))
+		default:
+			err = errors.New("Need explicit size")
+		}
+		if err != nil {
+			return err
+		}
+		return nil
+	},
+})
+
+var WriteStrCmd = registerCommand(&Command{
+	Name: "es",
+	Desc: "Write string to memory.",
+	Run: func(c *Context, addr uint64, str ...string) error {
+		sb := []byte(strings.Join(str, " "))
+		err := c.U.MemWrite(addr, sb)
+		if err != nil {
+			return err
 		}
 		return nil
 	},
